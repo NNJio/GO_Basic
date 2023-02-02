c6894c5848c06182e3928848f189bf2a5767f02a
NNJio <phatsakorn.newz@gmail.com>
2023-02-02 15:57:06 +0700

    [Edits] Og var

--- main.go
+++ main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Helle Go This't first ")
-	fmt.Println("NNJio")
+	var name string = "NNJio"
+	var age int = 25
+	var score float32 = 99.9
+	var isPass bool = true
+	fmt.Println("My name is ", name)
+	fmt.Println("Age = ", age)
+	fmt.Println("Score = ", score)
+	fmt.Println("Pass Exam = ", isPass)
 
 }
